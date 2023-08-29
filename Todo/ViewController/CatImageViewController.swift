8f267dc5018f932e3c91f2c4b9e9a60bf2d43d67
dohyun <thtree523@gmail.com>
2023-08-29 10:08:22 +0900

    FEATURE: 고양이 이미지를 보여줄 화면 UI 구성

---
+++ Todo/ViewController/CatImageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CatImageViewController.swift
+//  Todo
+//
+//  Created by 김도현 on 2023/08/29.
+//
+
+import UIKit
+
+class CatImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
